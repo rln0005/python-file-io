f696f496f1b2190fab7557fddc98a489e86c2f2e
Becca <rln0005@auburn.edu>
2021-03-29 10:28:06 -0700

    word_find.py updated with 'best practices'--added modules

--- word_find.py
+++ word_find.py
@@ -1,18 +1,42 @@
-import re
-print('Opening origin.txt')
-line_number = 0
-with open('origin.txt', 'r') as find_in_stream:
-    print('Opening word_find_output.txt')
-    with open('word_find_output.txt', 'w') as find_out_stream:
-        for line in find_in_stream:
-            line = line.strip()
-            line_number += 1
-            search_string = r"(.\Sherit\w+)"
-            search_obj = re.compile(search_string)
-            herit = search_obj.findall(line)
-            for found in herit:
-                find_out_stream.write('{0}\t{1}\n'.format(line_number, found))
-print("Done!")
-print('origin.txt is closed?', find_in_stream.closed)
-print('word_find_output.txt is closed?', find_out_stream.closed)
 
+### search a file line by line for a matching regex and generate an output file with the matches and line numbers
+import sys
+import re
+
+def regex_compile(search_string):
+    """
+    Used to compile regex search string to object.
+    ------------Parameters----------
+    search_string : str : a regex pattern string to be used to search for
+    ------------Yields--------------
+    A compiled regex object that can be used in regex matching commands.
+    """
+    search_obj = re.compile(search_string)
+    return search_obj
+
+def line_strip(input, output):
+    """
+    Strips input file into lines with line numbers and finds all occurrences of regex_compile and writes the line number and regex-matching word to output file.
+    ------------Parameters------------
+    input : str : a .txt file to be read from
+    output : str : a .txt file to be written to
+    ------------Yields----------------
+    Output file written with the line number and word that matches regex_compile from input
+    """
+    line_number = 0
+    for line in input:
+        line = line.strip()
+        line_number += 1
+        found = regex_compile(search_string).findall(line)
+        for i in found:
+            output.write('{0}\t{1}\n'.format(line_number, i))
+
+
+if __name__ == '__main__':
+    with open('origin.txt', 'r') as input:
+        with open('output.txt', 'w') as output:
+            search_string = r"(.\Sherit\w+)"
+            regex_compile(search_string)
+            line_strip(input, output)
+    input.closed
+    output.closed
